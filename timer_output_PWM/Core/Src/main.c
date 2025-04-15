4417918418ff9880eacd954a54cff9e8496fedf4
Mahmoud Abdulaal <mmh.00090@gmail.com>
2025-04-15 22:41:48 +0200

    some adjustments

--- timer_output_PWM/Core/Src/main.c
+++ timer_output_PWM/Core/Src/main.c
@@ -105,7 +105,7 @@ void SystemClock_Config_HSE(uint8_t clock_freq)
 			clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
 			clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
 
-			Flatency = FLASH_ACR_LATENCY_1WS;
+			//Flatency = FLASH_ACR_LATENCY_1WS;
 
 			break;
 		}
@@ -124,7 +124,7 @@ void SystemClock_Config_HSE(uint8_t clock_freq)
 			clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
 			clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
 
-			Flatency = FLASH_ACR_LATENCY_2WS;
+			//Flatency = FLASH_ACR_LATENCY_2WS;
 
 			break;
 		}
@@ -143,7 +143,7 @@ void SystemClock_Config_HSE(uint8_t clock_freq)
 			clk_init.APB1CLKDivider = RCC_HCLK_DIV4;
 			clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
 
-			Flatency = FLASH_ACR_LATENCY_3WS;
+			//Flatency = FLASH_ACR_LATENCY_3WS;
 
 			break;
 		}
@@ -204,9 +204,14 @@ void Timer2_init(void)
 
 	//the below period and prescaler is configures to give the value of timer_cnt_clck 10Khz
 	//so the timer clock freq is 50Mhz/prescaler + 1 = 10Khz so ARR = 10000
+
+	memset(&tim2PWM_config , 0 , sizeof(TIM_OC_InitTypeDef));
+
+	//for producing 1Khz signal
+
 	htimer2.Instance = TIM2;
-	htimer2.Init.Period = 10000-1;
-	htimer2.Init.Prescaler = 4999;
+	htimer2.Init.Period = 1000-1;
+	htimer2.Init.Prescaler = 49;
 
 	if(HAL_TIM_PWM_Init(&htimer2) != HAL_OK)
 	{
@@ -215,28 +220,28 @@ void Timer2_init(void)
 
 	tim2PWM_config.OCMode = TIM_OCMODE_PWM1;
 	tim2PWM_config.OCPolarity = TIM_OCPOLARITY_HIGH;
-	tim2PWM_config.Pulse = (htimer2.Init.Period *25)/100; //25% Duty cycle
+	tim2PWM_config.Pulse = (htimer2.Init.Period * 25)/100; //25% Duty cycle
 
 	if(HAL_TIM_PWM_ConfigChannel(&htimer2, &tim2PWM_config, TIM_CHANNEL_1)!= HAL_OK)
 	{
 		Error_handler();
 	}
 
-	tim2PWM_config.Pulse = (htimer2.Init.Period *25)/100; //25% Duty cycle
+	tim2PWM_config.Pulse = (htimer2.Init.Period * 45)/100; //45% Duty cycle
 
 	if(HAL_TIM_PWM_ConfigChannel(&htimer2, &tim2PWM_config, TIM_CHANNEL_2)!= HAL_OK)
 	{
 		Error_handler();
 	}
 
-	tim2PWM_config.Pulse = (htimer2.Init.Period *75)/100; //75% Duty cycle
+	tim2PWM_config.Pulse = (htimer2.Init.Period * 75)/100; //75% Duty cycle
 
 	if(HAL_TIM_PWM_ConfigChannel(&htimer2, &tim2PWM_config, TIM_CHANNEL_3)!= HAL_OK)
 	{
 		Error_handler();
 	}
 
-	tim2PWM_config.Pulse = (htimer2.Init.Period *95)/100; //95% Duty cycle
+	tim2PWM_config.Pulse = (htimer2.Init.Period * 95)/100; //95% Duty cycle
 
 	if(HAL_TIM_PWM_ConfigChannel(&htimer2, &tim2PWM_config, TIM_CHANNEL_4)!= HAL_OK)
 	{
