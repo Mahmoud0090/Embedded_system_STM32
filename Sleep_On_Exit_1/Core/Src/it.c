d9eae0ac83bd9f6791755f2f4670538e67d6d020
Mahmoud Abdulaal <mmh.00090@gmail.com>
2025-05-06 22:52:19 +0200

    USART2_CLK_SLEEP_DISABLE func

--- Sleep_On_Exit_1/Core/Src/it.c
+++ Sleep_On_Exit_1/Core/Src/it.c
@@ -14,11 +14,11 @@ void SysTick_Handler(void) //systick interrupt handler
 
 void TIM6_DAC_IRQHandler(void)
 {
-	__HAL_RCC_USART2_CLK_ENABLE();
+	//__HAL_RCC_USART2_CLK_ENABLE();
 	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET);
 	HAL_TIM_IRQHandler(&htimer6);
 	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
-	__HAL_RCC_USART2_CLK_DISABLE();
+	//__HAL_RCC_USART2_CLK_DISABLE();
 }
 
 void USART2_IRQHandler(void)
--- Sleep_On_Exit_1/Core/Src/msp.c
+++ Sleep_On_Exit_1/Core/Src/msp.c
@@ -36,6 +36,8 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart)
 
 	//1- enable the clock for tha USART2 peripheral as well as for GPIOA peripheral
 	__HAL_RCC_USART2_CLK_ENABLE();
+	__HAL_RCC_USART2_CLK_SLEEP_DISABLE();
+
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	//2- Do the pin muxing configuration
 	gpio_uart.Pin = GPIO_PIN_2;
